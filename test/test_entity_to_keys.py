d612cfb74a3358b51b7291fbab52a7d6d190f2d1
Chris Dent <chris.dent@gmail.com>
2013-09-13 21:53:28 +0100

    test keys for a single bag
    the resulting keys are the bag and the bag's tiddlers

--- test/test_entity_to_keys.py
+++ test/test_entity_to_keys.py
@@ -3,11 +3,15 @@ Test the creation of appropriate keys when:
 
 * a response to a GET is sent with Surrogate-Key headers
 * a PUT is made and store HOOKs need to purge the right stuff
+
+Verbosity in these tests is because of trying to work out what
+the right keys are.
 """
 
 
 from tiddlywebplugins.fastly.surrogates import entity_to_keys, uri_to_keys
 
+from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.tiddler import Tiddler
 
 
@@ -18,10 +22,26 @@ def test_tiddler_to_keys():
     tiddler = Tiddler('tidone', 'bagone')
 
     tiddler_key = 'T:bagone/tidone'
-    bag_key = 'BT:bagone'
+    bag_tiddler_key = 'BT:bagone'
 
     keys = entity_to_keys(tiddler)
 
     assert len(keys) == 2
     assert tiddler_key in keys
+    assert bag_tiddler_key in keys
+
+
+def test_bag_to_keys():
+    """
+    A single bag's keys are itself and it's tiddlers.
+    """
+    bag = Bag('bagone')
+
+    bag_key = 'B:bagone'
+    bag_tiddler_key = 'BT:bagone'
+
+    keys = entity_to_keys(bag)
+
+    assert len(keys) == 2
     assert bag_key in keys
+    assert bag_tiddler_key in keys
--- tiddlywebplugins/fastly/surrogates.py
+++ tiddlywebplugins/fastly/surrogates.py
@@ -1,5 +1,8 @@
 """
 Routines for generating surrogate keys.
+
+This will slowly evolve to be more declarative as it is basically
+just templates.
 """
 
 from tiddlyweb.util import superclass_name
@@ -19,11 +22,25 @@ def uri_to_keys(uri, environ):
     pass
 
 
+def bag_to_keys(bag):
+    """
+    Return keys for the bag itself and it's tiddlers.
+    """
+    return [bag_key(bag.name), bag_tiddler_key(bag.name)]
+
+
 def tiddler_to_keys(tiddler):
     """
     Return keys for the tiddler itself and the tiddler's bag's tiddlers.
     """
     return [tiddler_key(tiddler), bag_tiddler_key(tiddler.bag)]
+
+
+def bag_key(bag_name):
+    """
+    Key for a single bag.
+    """
+    return 'B:%s' % encode_name(bag_name)
 
 
 def tiddler_key(tiddler):
@@ -44,7 +61,7 @@ def bag_tiddler_key(bag_name):
 # XXX: this is rather naive and useless at the moment
 DISPATCH = {
     'tiddler': tiddler_to_keys,
-    #'bag': bag_to_keys,
+    'bag': bag_to_keys,
     #'recipe': recipe_to_keys,
 }
 
